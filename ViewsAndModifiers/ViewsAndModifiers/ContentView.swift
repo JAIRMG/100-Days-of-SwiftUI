b319214cbcc5a9eb4b5483e4ab75a545158d2d25
Jair Moreno Gaspar <jair.moreno-gaspar@external.flixbus.com>
2022-03-16 21:34:33 +0100

    Challenge 3, Views and Modifiers

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -2,15 +2,30 @@
 
 import SwiftUI
 
+struct CustomModifier: ViewModifier {
+  func body(content: Content) -> some View {
+    content
+      .font(.largeTitle)
+      .foregroundColor(.blue)
+  }
+}
+
+extension View {
+  func mainTitle() -> some View {
+    modifier(CustomModifier())
+  }
+}
+
 struct ContentView: View {
     var body: some View {
-      Button("Hello, world!") {
-        print(type(of: self.body))
+      VStack {
+        Button("Hello, world!") {
+          print(type(of: self.body))
+        }
+        .modifier(CustomModifier()) // 1st way of adding a custom modifier
+        Text("I am a title")
+          .mainTitle() // 2nd way of adding a custom modifier
       }
-      .frame(width: 200, height: 200)
-      .background(.red)
-      .padding()
-      .background(.purple)
     }
 }
 
