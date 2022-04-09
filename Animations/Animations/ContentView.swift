47331f0e9bbab08a88cf0a7fe6ab3b6a9879c6a0
Jair Moreno Gaspar <jair.moreno-gaspar@external.flixbus.com>
2022-04-09 17:10:45 +0200

    Day 32, Alarm button

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -3,9 +3,29 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var animationAmount: CGFloat = 1
+  
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+      Button("Tap me") {
+        // do nothing
+      }
+      .padding(50)
+      .background(.red)
+      .foregroundColor(.white)
+      .clipShape(Circle())
+      .overlay(
+        Circle()
+          .stroke(.red)
+          .scaleEffect(animationAmount)
+          .opacity(2 - animationAmount)
+          .animation(
+            .easeInOut(duration: 1)
+              .repeatForever(autoreverses: false),
+            value: animationAmount)
+      )
+      .onAppear {
+        animationAmount = 2
+      }
     }
 }
 
